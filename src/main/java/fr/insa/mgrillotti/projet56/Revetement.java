ec8b0ba76de3b801e1befd738dc1855a68df83ef
mgrillotti01 <mgrillotti01@pc-0287.csip.in>
2023-03-15 16:49:43 +0100

    Partage de tout le projet

---
+++ src/main/java/fr/insa/mgrillotti/projet56/Revetement.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.mgrillotti.projet56;
+
+/**
+ *
+ * @author thenry01
+ */
+public class Revetement {
+    // Attributs
+    int idRevetement;
+    double prix;
+    
+    Revetement(int id, double p)
+    {
+        // Constructeurs
+        this.idRevetement=id;
+        this.prix=p;
+    }
+}
