f7f43d91a0ed7a773c41dc92cecc607ea6afc7ff
lgoubet01 <127971101+lgoubet01@users.noreply.github.com>
2023-05-28 12:23:48 +0200

    Update Coin.java

--- src/main/java/fr/insa/mgrillotti/projet56/Coin.java
+++ src/main/java/fr/insa/mgrillotti/projet56/Coin.java
@@ -4,12 +4,32 @@
  */
 package fr.insa.mgrillotti.projet56;
 
+import java.awt.Color;
+
 /**
  *
  * @author Amadou Coulibaly
  */
 public class Coin {
-// test
+
+    // Attributs
+    public static double rayon = 5;
+    
+    private Color couleur;
+    private int idCoin;
+    private double cx;
+    private double cy;
+    
+    // Constructeur
+    Coin(int id, double x, double y)
+    {
+      this.idCoin=id;
+      this.cx=x;
+      this.cy=y;
+      this.couleur = Color.BLACK;
+    }
+    
+    // test
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -20,32 +40,47 @@ public class Coin {
         sb.append('}');
         return sb.toString();
     }
-    // Attributs
-    int idCoin;
-    double cx;
-    double cy;
-    // Constructeur
-    Coin(int id, double x, double y)
-    {
-      this.idCoin=id;
-      this.cx=x;
-      this.cy=y;
-    }
     
     void afficher()
     {
-        System.out.println(" Coin : id ="+this.idCoin+" abscisse = "+this.cx+ " Ordonnée ="+this.cy);
+        System.out.println(" Coin : id ="+this.getIdCoin()+" abscisse = "+this.getX()+ " Ordonnée ="+this.getY());
     }
     
-    int getID() {//récupérer id coin
+    public static Coin demandePoint() {
+        System.out.println("id :");
+        int id = Lire.i();
+        System.out.println("abscisse : ");
+        double cx = Lire.d();
+        System.out.println("ordonnée : ");
+        double cy = Lire.d();
+        return new Coin(id, cx, cy);
+    }
+    
+    /**
+     * @return the couleur
+     */
+    public Color getCouleur() {
+        return couleur;
+    }
+    
+    /**
+     * @return the idCoin
+     */
+    int getIdCoin() {
         return this.idCoin;    
     }
     
-    double getX() {//récupérer X coin
+    /**
+     * @return the cx
+     */
+    double getX() {
         return this.cx;    
     }
     
-    double getY() {//récupérer Y coin
+    /**
+     * @return the cy
+     */
+    double getY() {
         return this.cy;    
     }
     
